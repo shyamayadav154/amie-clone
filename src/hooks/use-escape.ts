8a9903d0c5146a45ccfc9beb31140b78882f187c
shyam <shyamayadav154@gmail.com>
2023-06-03 21:29:03 +0530

    feat: hooks added

---
+++ src/hooks/use-escape.ts
@@ -0,0 +1,16 @@
+
+import { useEffect } from "react";
+
+export const useEscapePress = (onEscapePress: () => void) => {
+  useEffect(() => {
+    const onKeyUp = (ev: KeyboardEvent) => {
+      if (ev.key === "Escape") onEscapePress();
+    };
+
+    window.addEventListener("keyup", onKeyUp);
+
+    return () => {
+      window.removeEventListener("keyup", onKeyUp);
+    };
+  }, [onEscapePress]);
+};
